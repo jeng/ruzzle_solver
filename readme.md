e6e38dd2895d60374d31c9f787d8ba55821057b6
Jeremy English <jhe@jeremyenglish.org>
2015-08-05 01:23:04 -0500

    Implemented indexed lookup
    Also removed globals.

--- readme.md
+++ readme.md
@@ -1,6 +1,41 @@
 ##Ruzzle Solver##
 
-If you like cheating at games here is a Ruzzle solver.  It is limited to
-six letter words to make it resonably fast.  That also allows for you to
-remain incognito.
+If you like cheating at games here is a Ruzzle solver.  
+
+It uses a backtracking algorithm to try every path.  With the data indexed this runs really fast.
+
+`Word.lst` came as data from some old ita puzzles.  It appears to be fairly complete.
+
+`prefix.idx` gets created from words_stats.py
+
+Example run:
+
+    RUZZLE SOLVER
+    Enter each line of the puzzle here
+    Do not put any space between the letters
+    > gstr
+    > uiew
+    > year
+    > itse
+    ae ai ait aits ar are ares ars arse as
+    at aw awe awee awes ear ears ease east eat
+    eats er era eras erase ere ers erst es et
+    eta etas gie gies git gits guest gust guy it
+    its ras rase rat rats raw rawer rawest re rear
+    rears ree rees reest rei res reseat resee resees reset
+    rest sea sear searer sears seas seat seats see seer
+    sees sei ser sera serai sere seres serest sers set
+    seta setae sew sewar sewars si siesta sit site staig
+    staigs star stare starer stares staw stere steres stew sties
+    street streets strew sty stye sue sues suet suets suety
+    suit suite suiter suiters ta tae tar tare tares tars
+    tas taw tawer tawers tea tear tears teas tease teat
+    teats tee tees terai terse tew ti tie tier tiers
+    ties treat treats treaty tree trees twa twae twaes twas
+    twat twats twee tweet tweets tye tyee tyees tyes us
+    use user users wae waes wait waiter waiters waits war
+    ware wares wars was wast wat wats we wear wears
+    wee wees weest weet weets were wert west wrest ye
+    yea year years yeas yeast yes yet yeti
+
 
--- ruzzle_solve.py
+++ ruzzle_solve.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import re
 
 DIRECTIONS = {'N': (0, 1), 'NE': (1, -1), 'E': (1, 0),
               'SE': (1, 1), 'S': (0, 1), 'SW': (-1, 1),
@@ -6,20 +7,24 @@ DIRECTIONS = {'N': (0, 1), 'NE': (1, -1), 'E': (1, 0),
     
 MAX_ROW = 4
 MAX_COLUMN = 4
-SMALL_WORD_LOOKUP_MAX = 6
+SMALL_WORD_LOOKUP_MAX = 5
 
-word = []
-small_word_lookup = {}
-word_prefix_index = {}
-solved_words = {}
+
+def slurp(file):
+    data = []
+    with open(file) as f:
+        for line in f:
+            data.append(line.rstrip())
+    return data
+
 
 def load_word_list():
-    word_list = []
-    with open("WORD.LST") as f:
-        for line in f:
-            word_list.append(line.rstrip())
-    return word_list
-        
+    return slurp("WORD.LST")
+
+
+def load_prefix_index():
+    return {x:int(y) for x, y in [x.split(" ") for x in slurp("prefix.idx")]}
+
 
 def move(pos, direction):
     x, y = pos
@@ -37,23 +42,62 @@ def in_range(pos):
     return 0 <= x < MAX_COLUMN and 0 <= y < MAX_ROW
 
 
-def find_words(start_pos, ruzzle, history, s):
-    #word_list = get_prefix_words(s)
+def get_prefix_words(word, word_list, prefix_index):
+    prefix = word[:SMALL_WORD_LOOKUP_MAX]
+    lookup = {}
+    if prefix in prefix_index:
+        line_number = prefix_index[prefix]
+        while (line_number < len(word_list)):
+            current_word = word_list[line_number]
+            if re.search("^" + prefix + "*", current_word):
+                lookup[current_word] = 0
+            else:
+                return lookup
+            line_number = line_number + 1
+    else:
+        return lookup
+    
 
-    global small_word_lookup
-    global solved_words
+def find_words(start_pos, ruzzle, history, s, result=None, complete_word_list=[], small_word_lookup={}, prefix_index_lookup={}):
     if 1 < len(s) <= SMALL_WORD_LOOKUP_MAX:
         if s in small_word_lookup:
-            if not s in solved_words:
-                #print s, history, [get(ruzzle, p) for p in history]
-                solved_words[s] = ""
+            result.append(s)
     elif len(s) > SMALL_WORD_LOOKUP_MAX:
-        return
+        prefix_word_list = get_prefix_words(s, complete_word_list, prefix_index_lookup)
+        if s in prefix_word_list:
+            result.append(s)
+        else:
+            return result
 
     for direction in DIRECTIONS:
         new_pos = move(start_pos, direction)
         if in_range(new_pos) and not new_pos in history:
-            find_words(new_pos, ruzzle, history[:] + [new_pos], s + get(ruzzle, new_pos))
+            result = find_words(new_pos, 
+                                ruzzle, history[:] + [new_pos], 
+                                s + get(ruzzle, new_pos), 
+                                result=result,
+                                small_word_lookup=small_word_lookup,
+                                complete_word_list=complete_word_list,
+                                prefix_index_lookup=prefix_index_lookup)
+
+    return result
+
+
+def unique_result(result_list, unique=[], history={}):
+    for element in result_list:
+        if type(element) is list:
+            unique = output_result(element, unique, history)
+        else:
+            if not element in history:
+                history[element] = ""
+                unique.append(element)
+    return unique
+
+
+def chunks(l, n):
+    """Yield successive n-sized chunks from l."""
+    for i in xrange(0, len(l), n):
+        yield l[i:i+n]
 
 
 def main():
@@ -61,41 +105,35 @@ def main():
     print "Enter each line of the puzzle here"
     print "Do not put any space between the letters"
 
-    ruzzle = []
-        
-    global words
     words = load_word_list()
-
     #create a dictionary for words <= 5
-    global small_word_lookup
-    for w in words:
-        if len(w) <= 6:
-            small_word_lookup[w] = ""
+    small_word_lookup = {w:0 for w in words if len(w) <= SMALL_WORD_LOOKUP_MAX}
 
     #load the index for longer words
-    global index_word
+    indexed_words = load_prefix_index()
 
     #get the puzzle layout
+    ruzzle = []
     for i in range(MAX_ROW):
         s = raw_input("> ")
         ruzzle.append(list(s))
 
     #solve
-    global solved_words
-    solved_words = {}
+    solved_words = []
     for y in range(MAX_ROW):
         for x in range(MAX_COLUMN):
-            find_words((x,y), ruzzle, [(x,y)], get(ruzzle, (x, y)))
+            solved_words = find_words((x,y), 
+                                      ruzzle, [(x,y)], 
+                                      get(ruzzle, (x, y)),
+                                      result = solved_words,
+                                      complete_word_list=words,
+                                      small_word_lookup=small_word_lookup,
+                                      prefix_index_lookup=indexed_words)
     
-    output_list = []
-    for word in solved_words:
-        output_list.append(word)
-        if len(output_list) > 9:
-            print " ".join(output_list)
-            output_list = []
-            
-    print " ".join(output_list)
-    
+    output_list = unique_result(solved_words)
+    for row in chunks(sorted(output_list), 10):
+        print " ".join(row)
+
 
 if __name__ == '__main__':
     main()
